c5ed6baff3f5cbb81fae8075c416e0e37cccb8e7
Juan Carlos <jcge97@gmail.com>
2022-10-18 14:01:01 +0100

    data/

--- code/seg_programa.R
+++ code/seg_programa.R
@@ -12,12 +12,20 @@ library(lubridate)
 
 ###### Seguimeinto general
 
-# url_seg_general <-
+url_seg_general <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/seguimiento.csv"
+seg_general <- read_csv(url_seg_general)
 
 ###### Seguimiento del ritmo por km
 
-# url_rit_km <- 
+url_rit_km <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/seg_ritmo.csv"
+rit_km <- read_csv(url_rit_km) 
 
 ###### Seguimiento del ritmo cardiaco
 
-#url_rit_card <-
+url_rit_card <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/seg_cardiaco.csv"
+rit_card <- read_csv(url_rit_card)
+
+###### Seguimiento del ritmo cardiaco
+
+url_sueno <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/sueno.csv"
+sueno <- read_csv(url_sueno)
