9afebc140a2c8f5f68cabc19b8c313af83807c05
Juan Carlos <jcge97@gmail.com>
2022-10-18 14:29:25 +0100

    data/

--- code/seg_programa.R
+++ code/seg_programa.R
@@ -8,24 +8,65 @@ library(readxl)
 library(glue)
 library(lubridate)
 
-# importar data
+#--------------------------------#
+# Importar y preprocesar mi data #
+#--------------------------------#
 
 ###### Seguimeinto general
 
 url_seg_general <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/seguimiento.csv"
-seg_general <- read_csv(url_seg_general)
+seg_general <- read_csv(url_seg_general) %>% 
+    mutate(
+        dia_semana=wday(fecha, label=TRUE, abbr = FALSE),
+        mes=month(fecha, label=TRUE, abbr = FALSE),
+        anio=year(fecha),
+        hoy=today(),
+        es_hoy=fecha == hoy
+    )
+
+# seg_general[,c("dia_semana","anio","mes","hoy", "fecha", "es_hoy")] %>% view()
 
 ###### Seguimiento del ritmo por km
 
 url_rit_km <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/seg_ritmo.csv"
-rit_km <- read_csv(url_rit_km) 
+rit_km <- read_csv(url_rit_km) %>% 
+    mutate(
+      dia_semana=wday(fecha, label=TRUE, abbr = FALSE),
+        mes=month(fecha, label=TRUE, abbr = FALSE),
+        anio=year(fecha),
+        hoy=today(),
+        es_hoy=fecha == hoy  
+    )
+
+# rit_km %>% view()
 
 ###### Seguimiento del ritmo cardiaco
 
 url_rit_card <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/seg_cardiaco.csv"
-rit_card <- read_csv(url_rit_card)
+rit_card <- read_csv(url_rit_card) %>% 
+    mutate(
+        dia_semana=wday(fecha, label=TRUE, abbr = FALSE),
+        mes=month(fecha, label=TRUE, abbr = FALSE),
+        anio=year(fecha),
+        hoy=today(),
+        es_hoy=fecha == hoy
+    )
+
+# rit_card %>% view()
 
 ###### Seguimiento del ritmo cardiaco
 
-url_sueno <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/sueno.csv"
-sueno <- read_csv(url_sueno)
+url_suenio <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/sueno.csv"
+suenio <- read_csv(url_sueno) %>% 
+    mutate(
+        dia_semana=wday(fecha, label=TRUE, abbr = FALSE),
+        mes=month(fecha, label=TRUE, abbr = FALSE),
+        anio=year(fecha),
+        hoy=today(),
+        es_hoy=fecha == hoy
+    )
+
+# suenio %>% view()
+
+
+
