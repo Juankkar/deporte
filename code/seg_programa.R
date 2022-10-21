edb1e985802ff068f918ae81a42b545adb7ca28b
Juan Carlos <jcge97@gmail.com>
2022-10-21 13:48:17 +0100

    code/

--- code/seg_programa.R
+++ code/seg_programa.R
@@ -14,6 +14,10 @@ library(cowplot)
 # Importar y preprocesar mi data #
 #--------------------------------#
 
+fake_data1 <- tibble(x=rep(c(1:50),4),
+                     y=rep(c(1:50),4),
+                     grupos=c(rep("grupo1",50),rep("grupo2",50), rep("grupo3",50),rep("grupo4",50)))
+
 ###### Seguimeinto general
 
 url_seg_general <- "https://raw.githubusercontent.com/Juankkar/deporte/main/data/seguimiento.csv"
@@ -80,6 +84,14 @@ suenio <- read_csv(url_sueno) %>%
 
 ### Km recorridos según el día de la semana.
 
+estadisticos_km <- seg_general %>%
+    filter(actividad == "correr") %>%
+    summarise(media_km=round(mean(km_recorrido, na.rm=TRUE),2),
+              sd_km=round(sd(km_recorrido, na.rm=TRUE),2))
+dias_correr_km <- seg_general %>%
+    group_by(actividad) %>%
+    count()
+
 km <- seg_general %>%
     ggplot(aes(dia_semana, km_recorrido,fill=es_hoy,
                size=es_hoy, group=semana)) +
@@ -115,6 +127,13 @@ km <- seg_general %>%
     )
 
 ### Tiempo de recorrido
+estadisticos_tiempo <- seg_general %>%
+    filter(actividad == "correr") %>%
+    mutate(tiempo_sec=as.numeric(tiempo_min)) %>%
+    select(tiempo_sec) %>%
+    summarise(media_tiempo=round(mean(tiempo_sec, na.rm=TRUE)/60,2),
+              sd_tiempo=round(sd(tiempo_sec, na.rm=TRUE)/60,2))
+
 tiempo <- seg_general %>%
     ggplot(aes(dia_semana,tiempo_min,fill=es_hoy,
                size=es_hoy, group=semana)) +
@@ -151,6 +170,13 @@ tiempo <- seg_general %>%
 
 ## Ritmo promedio 
 
+estadisticos_ritmo <- seg_general %>%
+    filter(actividad == "correr") %>%
+    mutate(ritmo_medio_sec=as.numeric(ritmo_medio_min)) %>%
+    select(ritmo_medio_sec) %>%
+    summarise(media_ritmo=round(mean(ritmo_medio_sec, na.rm=TRUE)/60,2),
+              sd_ritmo=round(sd(ritmo_medio_sec, na.rm=TRUE)/60,2))
+
 ritmo_promedio <- seg_general %>%
     ggplot(aes(dia_semana,ritmo_medio_min,fill=es_hoy,
                size=es_hoy, group=semana)) +
@@ -185,6 +211,13 @@ ritmo_promedio <- seg_general %>%
     )
 
 ## Ritmo máximo 
+
+estadisticos_ritmo_max <- seg_general %>%
+    filter(actividad == "correr") %>%
+    mutate(ritmo_max_sec=as.numeric(ritmo_max_min)) %>%
+    select(ritmo_max_sec) %>%
+    summarise(media_ritmo=round(mean(ritmo_max_sec, na.rm=TRUE)/60,2),
+              sd_ritmo=round(sd(ritmo_max_sec, na.rm=TRUE)/60),7)
 
 ritmo_max <- seg_general %>%
     ggplot(aes(dia_semana,ritmo_max_min,fill=es_hoy,
@@ -225,3 +258,42 @@ plot_grid(km, tiempo, ritmo_promedio, ritmo_max,
 ggsave("ritmo_promedio.png", path="C:\\Users\\jcge9\\Desktop\\deporte\\graficas",
         width = 10, height = 8)
 
+fake_etiquetas1 <- tibble(
+    x=c(rep(25,9)),
+    y=c(30,25,20,rep(c(28,24),3)),
+    label=c(glue("Promedio: {estadisticos_km$media_km} Km"), glue("Desviación típica: {estadisticos_km$sd_km} Km"),
+            glue("Número de carreras: {dias_correr_km$n}"), glue("Promedio: {estadisticos_tiempo$media_tiempo} min"),
+            glue("Desviación típica: {estadisticos_tiempo$sd_tiempo} min"),glue("Promedio: {estadisticos_ritmo$media_ritmo} min"),
+            glue("desviación típica: {estadisticos_ritmo$media_ritmo} min"),glue("Promedio: {estadisticos_ritmo_max$media_ritmo} min"),
+            glue("Desviación típica: {estadisticos_ritmo_max$sd_ritmo} min")),
+    grupos=c(rep("Distancia recorrida (km)",3), rep("Tiempo de recorrido (min)",2), 
+             rep("Ritmo promedio (min)",2), rep("Ritmo maximo (min)",2))
+)
+
+estadisticos_ritmo_max$sd_ritmo
+
+fake_data1 %>%
+    mutate(
+        grupos=factor(grupos,
+                      levels = c("grupo1", "grupo2", "grupo3", "grupo4"),
+                      labels = c("Distancia recorrida (km)", "Tiempo de recorrido (min)",
+                                 "Ritmo promedio (min)", "Ritmo maximo (min)"))
+    ) %>%
+    ggplot(aes(x,y)) +
+    facet_wrap(~grupos,ncol=2) +
+    geom_text(data = fake_etiquetas1, aes(x,y,label=label), inherit.aes = FALSE,
+              size=6) +
+    labs(
+        x=NULL, y=NULL
+    ) +
+    scale_y_continuous(limits = c(10,40)) +
+    theme_classic() +
+    theme(
+        strip.text = element_text(size = 15),
+        axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank()
+    )
+
+ggsave("C:\\Users\\jcge9\\Desktop\\deporte\\graficas\\estadisticas1.png",
+       width=8, height = 6)
